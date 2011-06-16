0aad077520a2466d79bbc21049a19363bc372dc8
Bartholomäus Dedersen <2011@selbstapotheose.de>
2011-06-16 21:15:11 +0200

    Write enabled + stop function

--- serial_reader.py
+++ serial_reader.py
@@ -21,32 +21,74 @@ class SerialReader:
     def reader(self):
         """loop forever and copy serial->command i/o"""
         while self.alive:
-                data = self.serial.read(1)              # read one, blocking
-                n = self.serial.inWaiting()             # look if there is more
-                if n:
-                    data = data + self.serial.read(n)   # and get as much as possible
-                if data:
-                    # the spy shows what's on the serial port, so log it before converting newlines
-                    if True:
-                        sys.stdout.write(codecs.escape_encode(data)[0])
-                        sys.stdout.flush()
-                    '''                    
-                    if self.ser_newline and self.net_newline:
-                        # do the newline conversion
-                        # XXX fails for CR+LF in input when it is cut in half at the begin or end of the string
-                        data = net_newline.join(data.split(ser_newline))
-                    # escape outgoing data when needed (Telnet IAC (0xff) character)
-                    self._write_lock.acquire()                    
-                    try:
-                        self.socket.sendall(data)           # send it over TCP
-                    finally:
-                        self._write_lock.release()
-                        '''
-	self.alive = False
-	
+            data = self.serial.read(1)              # read one, blocking
+            n = self.serial.inWaiting()             # look if there is more
+            if n:
+                data = data + self.serial.read(n)   # and get as much as possible
+            if data:
+                # verbosity for copying the serial data to the console
+                if self.verbose:
+                    sys.stdout.write(codecs.escape_encode(data)[0])
+                    sys.stdout.flush()
+                '''                    
+                # send the data to the listing component
+                self._write_lock.acquire()                    
+                try:
+                    self.socket.sendall(data)           # send it over TCP
+                finally:
+                    self._write_lock.release()
+                '''
+        self.alive = False
+
+
+    def writer(self):
+        """loop forever and copy socket->serial"""
+        while self.alive:
+            #Read data from reading component
+            #data = self.socket.recv(1024)
+            if not data:
+                break
+            self.serial.write(data)                 # get a bunch of bytes and send them
+            # the spy shows what's on the serial port, so log it after converting newlines
+            if self.verbose:
+                sys.stdout.write(codecs.escape_encode(data)[0])
+                sys.stdout.flush()
+        self.alive = False
+        self.thread_read.join()
+
+    def stop(self):
+        """Stop copying"""
+        if self.alive:
+            self.alive = False
+            self.thread_read.join()
+
+    
 
 #if __name__ == '__main__':
 # connect to serial port
+
+#Commandline parsing
+import optparse
+
+parser = optparse.OptionParser(
+    usage = "%prog [options]",
+    description = "Wireless Sensor Network to TCP bridge with puffering",
+    epilog = """\
+NOTE: no security measures are implemented. Anyone can remotely connect
+to this service over the network.
+
+Only one connection at once is supported. When the connection is terminated
+it waits for the next connect.
+""")
+
+parser.add_option("--v",
+        dest = "verbose",
+        action = "store_true",
+        help = "write all serial in- and output to the console",
+        default = False
+    )
+
+
 ser = serial.Serial()
 ser.port     = "/dev/ttyUSB1"
 ser.baudrate = 38400
@@ -63,7 +105,7 @@ while True:
     try:
         s = SerialReader(ser)
         s.shortcut()
-	time.sleep(1)
+        time.sleep(1)
     except KeyboardInterrupt:
         break
 
