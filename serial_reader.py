7f69dc12d291a46ee0d01b1974a08a9f23349a9e
Bartholomäus Dedersen <2011@selbstapotheose.de>
2011-06-16 12:41:11 +0200

    Basisversion Serial

---
+++ serial_reader.py
@@ -0,0 +1,60 @@
+import serial
+import thread
+import sys
+
+class SerialReader:
+    def shortcut(self):
+        """connect the serial port to the TCP port by copying everything
+           from one side to the other"""
+        self.alive = True
+        self.thread_read = threading.Thread(target=self.reader)
+        self.thread_read.setDaemon(True)
+        self.thread_read.setName('socket reader')
+        self.thread_read.start()
+        
+
+    def reader(self):
+        """loop forever and copy serial->socket"""
+        while self.alive:
+            try:
+                data = self.serial.read(1)              # read one, blocking
+                n = self.serial.inWaiting()             # look if there is more
+                if n:
+                    data = data + self.serial.read(n)   # and get as much as possible
+                if data:
+                    # the spy shows what's on the serial port, so log it before converting newlines
+                    if self.spy:
+                        sys.stdout.write(codecs.escape_encode(data)[0])
+                        sys.stdout.flush()
+                    if self.ser_newline and self.net_newline:
+                        # do the newline conversion
+                        # XXX fails for CR+LF in input when it is cut in half at the begin or end of the string
+                        data = net_newline.join(data.split(ser_newline))
+                    # escape outgoing data when needed (Telnet IAC (0xff) character)
+
+                    '''                    
+                    self._write_lock.acquire()                    
+                    try:
+                        self.socket.sendall(data)           # send it over TCP
+                    finally:
+                        self._write_lock.release()
+                        '''
+            except socket.error, msg:
+                sys.stderr.write('ERROR: %s\n' % msg)
+                # probably got disconnected
+                break
+        self.alive = False
+
+# connect to serial port
+ser = serial.Serial()
+ser.port     = "/dev/ttyUSB0"
+ser.baudrate = 38400
+
+
+try:
+    ser.open()
+except serial.SerialException, e:
+    sys.stderr.write("Could not open serial port %s: %s\n" % (ser.portstr, e))
+    sys.exit(1)
+    
+    
