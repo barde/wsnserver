47a35e881262b448a6bbcfa432bd0ec454a93c16
Kamil Wozniak <kamilski@gmx.de>
2011-06-22 11:23:42 +0200

    Added methods, which need to be implemented.

--- Controller.py
+++ Controller.py
@@ -4,7 +4,26 @@ Created on 22.06.2011
 '''
 
 class Controller(object):
-
+    
+    ''' 
+    Only the WSN should request this action. 
+    ReadCMDAction looks into the database for new CMDs
+    '''
+    def readCMDAction(self):
+        pass
+    
+    def saveCMDAction(self):
+        pass
+    
+    def readAllAction(self):
+        pass
+    
+    def readLeatestAction(self):
+        pass
+    
+    def removeAllAction(self):
+        pass
+        
     def __init__(self):
         '''
         Constructor
