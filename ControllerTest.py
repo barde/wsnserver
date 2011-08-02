b16a5bcb55de215b32a5c0ee4200a45f81a9431e
Kamil Wozniak <kamilski@gmx.de>
2011-08-02 18:32:52 +0200

    Added test for controller

---
+++ ControllerTest.py
@@ -0,0 +1,16 @@
+'''
+Created on 02.08.2011
+@author: Kamil Wozniak
+'''
+import unittest
+import Controller
+
+class ControllerTest(unittest.TestCase):
+
+    def testName(self):
+        pass
+
+
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.testName']
+    unittest.main()
