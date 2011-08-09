8b42f5e3bc374b8af0c2c8a82177581f21bd5819
Bartek Dedersen <debar@wsnserver.hrz.isc.fh-kiel.de>
2011-08-09 18:45:55 +0200

    Serial Reader minimal Version

--- serial_reader.py
+++ serial_reader.py
@@ -5,8 +5,11 @@ import codecs
 import time
 import glob
 
+import Controller
+
 class SerialReader:
     def __init__(self, verbose=True):
+	self.controllerId = ''
 	self.verbose = verbose
 
         self.serial = serial.Serial()
@@ -16,19 +19,26 @@ class SerialReader:
         try:
             self.serial.open()
         except serial.SerialException, e:
-            sys.stderr.write("Could not open serial port %s: %s\n" % (ser.portstr, e))
+            sys.stderr.write("Could not open serial port %s: %s\n" % (self.serial.portstr, e))
             sys.exit(1)
+
+	self.controller = Controller.Controller()
         
     def shortcut(self):
-        """connect the serial port to the TCP port by copying everything
-           from one side to the other"""
-        self.alive = True
-        self._write_lock = threading.Lock()
+	if self.controllerId.count == 0:
+		self.write("+WPANID")
+		self.controllerId = self.reader()
+		return
 
-        self.thread_read = threading.Thread(target=self.reader)
-        self.thread_read.setDaemon(True)
-        self.thread_read.setName('socket reader')
-        self.thread_read.start()
+	#receive data
+	outdata = self.reader()
+	if outdata:
+		self.controller.saveDataAction(controllerId, outdata)
+	#send data
+	indata = self.controller.readCMD(controllerId);
+	if indata:
+		self.write(indata)
+		
 
     def find_port(self):
         port = glob.glob('/dev/ttyUSB*')
@@ -39,53 +49,25 @@ class SerialReader:
             return port[0]
         
     def reader(self):
-        """loop forever and copy serial->command i/o"""
-        while self.alive:
-            self._write_lock.acquire()
-            data = self.serial.read(1)              # read one, blocking
-            n = self.serial.inWaiting()             # look if there is more
-            if n:
-                data = data + self.serial.read(n)   # and get as much as possible
-            if data:
-                # verbosity for copying the serial data to the console
-                if self.verbose:
-                    sys.stdout.write(codecs.escape_encode(data)[0])
-                    sys.stdout.flush()
-                '''                    
-                # send the data to the listing component
-                self._write_lock.acquire()                    
-                try:
-                    self.socket.sendall(data)           # send it over TCP
-                finally:
-                    self._write_lock.release()
-                '''
-            self._write_lock.release()
-        self.alive = False
+	data = ''
+	while self.serial.inWaiting() > 0:
+		data += self.serial.read(1)
+	if self.verbose:
+		sys.stdout.write("input:" + data)
+		sys.stdout.flush()
+	return data
+
+	
 
 
-    def writer(self):
+    def write(self,data):
         """loop forever and copy socket->serial"""
         while self.alive:
-            #Read data from reading component
-            #data = self.socket.recv(1024)
-            if not data:
-                break
             self.serial.write(data)                 # get a bunch of bytes and send them
-            # the spy shows what's on the serial port, so log it after converting newlines
+            # Verbose mode prints everthing to console
             if self.verbose:
-                sys.stdout.write(codecs.escape_encode(data)[0])
+                sys.stdout.write("output:" + data)
                 sys.stdout.flush()
-        self.alive = False
-        self.thread_read.join()
-
-    def stop(self):
-        """Stop copying"""
-        if self.alive:
-            self.alive = False
-            self.thread_read.join()
-
-    
-
 
 if __name__ == '__main__':
 # connect to serial port and do some test if not run as module
@@ -104,7 +86,7 @@ if __name__ == '__main__':
         it waits for the next connect.
         """)
 
-    parser.add_option("--v",
+    parser.add_option("-v",
         dest = "verbose",
         action = "store_true",
         help = "write all serial in- and output to the console",
@@ -119,8 +101,6 @@ if __name__ == '__main__':
     while True:
         try:
             s.shortcut()
-            #Put the stuff into database
-            s.stop()
         except KeyboardInterrupt:
             break
 
