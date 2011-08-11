2b26ad86fdaf4279da9f1d469d0f42300ecbfab1
Bartek Dedersen <debar@arete.fritz.box>
2011-08-11 09:25:39 +0200

    LazyData changed to another format of Singleton SerialReader's command line option stubs filled Fix with username

--- LazyData.py
+++ LazyData.py
@@ -1,13 +1,45 @@
 #!/usr/bin/env python
 
 #Lazy Data Singleton by Bartholomaeus Dedersen
+#based on Alan Felice work
+#Thread-safeness useless in project but kept
+#for future expanse
 
-class LazyData:
-        __lazyData = ""
-        def _call(self):
-                return self
-        def getData(self):
-                return self.__lazyData
-        def setData(self,data):
-                __lazyData = data
-        content = property(getData, setData)
+import thread
+
+class LazyData(object):
+    '''Implement Pattern: SINGLETON'''
+
+    __lockObj = thread.allocate_lock()  # lock object
+    __instance = None  # the unique instance
+    __content = ""
+
+    def __new__(cls, *args, **kargs):
+        return cls.getInstance(cls, *args, **kargs)
+
+    def __init__(self):
+        pass
+
+    def getInstance(cls, *args, **kargs):
+        '''Static method to have a reference to **THE UNIQUE** instance'''
+        # Critical section start
+        cls.__lockObj.acquire()
+        try:
+            if cls.__instance is None:
+                # (Some exception may be thrown...)
+                # Initialize **the unique** instance
+                cls.__instance = object.__new__(cls, *args, **kargs)
+        finally:
+            #  Exit from critical section whatever happens
+            cls.__lockObj.release()
+        # Critical section end
+
+        return cls.__instance
+
+    def getData(self):
+        return self.__content
+    def setData(self,data):
+        self.__content = data
+
+    content = property(getData, setData)
+    getInstance = classmethod(getInstance)
