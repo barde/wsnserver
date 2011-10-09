152d4f3313303842a39e85418e8e3773f26d9795
Kamil Wozniak <kamilski@gmx.de>
2011-10-09 19:45:51 +0200

    Added config parser to the dbconnection class. This class will be able to choose between different database configuration.

--- DBConnection.py
+++ DBConnection.py
@@ -4,17 +4,27 @@ Created on 09.10.2011
 @author: Kamil Wozniak
 '''
 import sqlite3
+import ConfigParser
 
 class DBConnection(object):
     '''
     classdocs
     '''
     
+    type = None
+    
     def getDBConnection(self):
-        return sqlite3.connect("data.db")
+        if self.type == 'sqlite3':
+            conn = sqlite3.connect("data.db")
+            conn.text_factory = str
+            return conn
 
     def __init__(self):
         '''
         Constructor
-        '''
+        '''  
+        config = ConfigParser.RawConfigParser()
+        config.read('wsn.cfg')
+        
+        self.type = config.get('Database-Config', 'type')
         
