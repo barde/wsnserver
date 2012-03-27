22213bc68f6ec9e5016e896e4b8bffa4371936e4
Bartek Dedersen <git@selbstapotheose.de>
2012-03-27 16:30:07 +0200

    doc holes filled

--- Dokumentation/clean-folder.sh
+++ Dokumentation/clean-folder.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-rm missfont.log wsnserver.aux wsnserver.lof wsnserver.log wsnserver.lot wsnserver.out wsnserver.toc
+rm missfont.log wsnserver.aux wsnserver.lof wsnserver.log wsnserver.lot wsnserver.out wsnserver.toc *.bbl *.blg
